770644313ff84910e4767bf6db330ed3f9042437
Sw <86825656+swlee9087@users.noreply.github.com>
2021-11-01 16:38:12 +0900

    16

--- frontend/src/common/components/Layout-common.js
+++ frontend/src/common/components/Layout-common.js
@@ -1,15 +1,12 @@
-import DemoContent from '@fuse/core/DemoContent';
 import { styled } from '@mui/material/styles';
-import FusePageSimple from '@fuse/core/FusePageSimple';
-import { useTranslation } from 'react-i18next';
 
-const Root = styled(FusePageSimple)({
-  '& .FusePageSimple-header': {},
-  '& .FusePageSimple-toolbar': {},
-  '& .FusePageSimple-content': {},
-  '& .FusePageSimple-sidebarHeader': {},
-  '& .FusePageSimple-sidebarContent': {},
-});
+// const Root = styled(FusePageSimple)({
+//   '& .FusePageSimple-header': {},
+//   '& .FusePageSimple-toolbar': {},
+//   '& .FusePageSimple-content': {},
+//   '& .FusePageSimple-sidebarHeader': {},
+//   '& .FusePageSimple-sidebarContent': {},
+// });
 
 function ExamplePage(props) {
   const { t } = useTranslation('examplePage');
